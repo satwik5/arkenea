32d34ad37df2bb1fdd5a019bdf8f2ad312f1635c
satwik5 <satwikgorlamandala@gmail.com>
2020-12-22 23:53:21 +0530

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -5,7 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { HashtagPipe } from './hashtag.pipe';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ListComponent } from './list/list.component';
@@ -14,7 +13,6 @@ import { ListComponent } from './list/list.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent,
     HashtagPipe,
     DashboardComponent,
     ListComponent
