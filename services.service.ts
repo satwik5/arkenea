589e1da2c496a575e914c418b4d021a15ab26c28
satwik5 <satwikgorlamandala@gmail.com>
2020-12-22 23:55:02 +0530

    Update services.service.ts

--- services.service.ts
+++ services.service.ts
@@ -30,16 +30,7 @@ export class ServicesService {
         catchError(this.handleError)
     ); 
   };
-  addProduct(data){
-    return this._http.post<any>(`${baseUrl}/addmoment`,{moment:data},{withCredentials:true})
-     .pipe( 
-        map(
-          (res)=> { //console.log(res)
-            return res['status']; 
-          }),
-          catchError(this.handleError)
-      ); 
-  }
+
   uploadImage(image){
     return this._http.post(`${baseUrl}/file`,image,{withCredentials:true})
     .pipe( 
@@ -60,26 +51,7 @@ export class ServicesService {
          catchError(this.handleError)
      ); 
   }
-  editProduct(data, id){
-    return this._http.post<any>(`${baseUrl}/editmoment`,{moment:data, id:id},{withCredentials:true})
-     .pipe( 
-        map(
-          (res)=> { //console.log(res)
-            return res['status']; 
-          }),
-          catchError(this.handleError)
-      ); 
-  }
-  getMoments(){
-    return this._http.get<any>(`${baseUrl}/getmoments`)
-    .pipe( 
-       map(
-         (res)=> { //console.log(res)
-           return res; 
-         }),
-         catchError(this.handleError)
-     ); 
-  }
+
   getCredentials(){
     return this._http.get<any>(`${baseUrl}/getcredentials`)
     .pipe( 
