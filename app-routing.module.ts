5790eac4a756e55588bfdcf35e0da31b6cb40104
satwik5 <satwikgorlamandala@gmail.com>
2020-12-22 23:52:52 +0530

    Update app-routing.module.ts

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth.guard';
-import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
 import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent , canActivate:[AuthGuard]},
   {path: 'list', component: ListComponent, canActivate:[AuthGuard]},
   {path: 'login', component: LoginComponent}
 ];
